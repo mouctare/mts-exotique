4f0378ea443bd657395870179311e19e39d46670
mouctare <mouctard78@gmail.com>
2021-08-12 16:43:13 +0200

    Ajout du component cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
 
@@ -10,13 +11,13 @@ function App() {
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/">
+          <Link className="brand" to="/">
             mts-exotique
-          </a>
+          </Link>
         </div>
         <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+          <Link to="/cart">Cart</Link>
+          <Link to="/signin">Sign In</Link>
         </div>
       </header>
       <main>
